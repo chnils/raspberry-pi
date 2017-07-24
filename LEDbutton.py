45a6469c831ca322c2860f94538d80011968ddda
chnils <nils_olsson94@hotmail.com>
2017-07-24 20:08:32 +0200

    tjo

--- LEDbutton.py
+++ LEDbutton.py
@@ -4,16 +4,16 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(22,GPIO.IN)
+GPIO.setup(18,GPIO.IN)
 GPIO.setup(24,GPIO.OUT)
-GPIO.setup(25,GPIO.OUT)
+#GPIO.setup(25,GPIO.OUT)
 
-#GPIO.output(25,GPIO.IN, Pull_Up_)
+#GPIO.output(25,GPIO.HIGH)
 while True:
 	
-	if  (GPIO.input(22)):
+	if  GPIO.input(18):
 		GPIO.output(24,GPIO.HIGH)
-		print "hej"
+		print("hej")
 	else: 
 		GPIO.output(24,GPIO.LOW)
 
