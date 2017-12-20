7b1117faf8ed8e414199ce5fa38e6b688c463766
chnils <nils_olsson94@hotmail.com>
2017-12-20 23:18:08 +0100

    motortest 1

--- motortest.py
+++ motortest.py
@@ -5,7 +5,7 @@ GPIO.setup(5,GPIO.OUT)   #Left motor input A
 GPIO.setup(7,GPIO.OUT)   #Left motor input B
 GPIO.setup(11,GPIO.OUT)  #Right motor input A
 GPIO.setup(13,GPIO.OUT)  #Right motor input B
-GPIO.setup(18,GPIO.IN, pull_up_down=GPIO.PUD_UP) #button
+GPIO.setup(12,GPIO.IN, pull_up_down=GPIO.PUD_UP) #button
 GPIO.setwarnings(False)
 
 a = False
@@ -23,16 +23,32 @@ while True:
         time.sleep(0.1)
 
     while(a):
-            print "Rotating both motors in clockwise direction"
-            GPIO.output(5,1)
+            print "Rotating right motor forwards"
+            GPIO.output(5,0)
             GPIO.output(7,0)
             GPIO.output(11,1)
             GPIO.output(13,0)
-            time.sleep(1)     #One second delay
+            time.sleep(2)     #One second delay
 
-            print "Rotating both motors in anticlockwise direction"
+            print "Rotating right motor backwards"
             GPIO.output(5,0)
-            GPIO.output(7,1)
+            GPIO.output(7,0)
             GPIO.output(11,0)
             GPIO.output(13,1)
-            time.sleep(1)
+            time.sleep(2)
+
+            print
+            "Rotating left motor forwards"
+            GPIO.output(5, 1)
+            GPIO.output(7, 0)
+            GPIO.output(11, 0)
+            GPIO.output(13, 0)
+            time.sleep(2)
+
+            print
+            "Rotating left motor backwards"
+            GPIO.output(5, 0)
+            GPIO.output(7, 1)
+            GPIO.output(11, 0)
+            GPIO.output(13, 0)
+            time.sleep(2)
